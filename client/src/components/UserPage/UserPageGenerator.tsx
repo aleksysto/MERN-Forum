a9e4416fdd777ede2356f004e351e03cddf04a41
aleksysto <a.stocki.060@studms.ug.edu.pl>
2024-01-25 01:10:21 +0100

    Server photo directory cleanup, added report button to user pages

--- client/src/components/UserPage/UserPageGenerator.tsx
+++ client/src/components/UserPage/UserPageGenerator.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { UserObject, UserProfileObject } from '../interfaces/UserObjectContext'
 import UserTableRow from './UserTableRow'
 import UserPostList from './UserPostList'
+import ReportButton from '../utils/ReportButton'
 
 export default function UserPageGenerator({ user }: { user: UserObject }): JSX.Element {
+    const [reported, setReported] = useState<boolean>(false)
     const data: UserProfileObject =
     {
         login: user.login,
@@ -12,13 +14,19 @@ export default function UserPageGenerator({ user }: { user: UserObject }): JSX.E
         lastActive: user.lastActive,
         entryDate: user.entryDate
     }
-    console.log()
+    useEffect(() => {
+        setReported(false)
+    }, [user])
     return (
         <>
             <div className="UserPageTable">
+                <div className="AccountPageReport">
+                    {reported ? <div className="pt-2 mr-1 underline ">User reported</div> : <ReportButton reported={reported} setReported={setReported} type={'user'} id={user._id} />}
+                </div>
                 <div className="AccountPageImage">
                     <img src={`http://localhost:4000/api/getImage/${user.profilePicture}`} alt="avatar" />
                 </div>
+
                 <div className="AccountPageTableBody">
                     <table>
                         <tbody>
